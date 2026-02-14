0f4dacc99ffb558e6f3004e099687e6ab60a8686
netogu <ernesto.gonzalez.u@gmail.com>
2026-02-13 18:21:04 -0800

    Fixed issues with CMake

--- include/wavegen.h
+++ include/wavegen.h
@@ -5,22 +5,6 @@
 
 namespace Scope {
 
-template <typename T> static void generate_waveform(Channel &c, T wave_func) {
-  for (int i = 0; i < c.trace.size(); ++i) {
-    const float dx = 0.001;
-    float time = static_cast<float>(i) * dx;
-    c.trace.add_sample(time, wave_func(time));
-  }
-}
-
-static float sinewave_1(float x) {
-  return 0.5f + 0.5f * sinf(50 * (x + (float)ImGui::GetTime() / 20));
-}
-
-static float sinewave_2(float x) {
-  return 0.6f + 0.5f * sinf(30 * (x + (float)ImGui::GetTime() / 18));
-}
-
 class WaveformGenerator {
 public:
   WaveformGenerator() {
